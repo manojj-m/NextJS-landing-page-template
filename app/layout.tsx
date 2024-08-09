36b4f32d40c4c9d402dd7ed77824986cc8a2d860
Manojj Ranjan <manojj.ranjan@gmail.com>
2024-08-09 02:50:23 +0200

    Modfied website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Convo | Language Learning",
-  description: "Speech focused language learning",
+  title: "Evaluate Learning",
+  description: "Personalised Feedback for Assignments",
 };
 
 export default function RootLayout({
